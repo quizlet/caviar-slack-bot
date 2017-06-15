5076360f614fcbd9c90057af8f0d7a6f7329d547
Eric Bomgardner <eric.bomgardner@quizlet.com>
2017-06-14 22:25:01 -0700

    Adding ability to send messages to channel without "@here"

--- lib/services/slack.rb
+++ lib/services/slack.rb
@@ -9,17 +9,20 @@ class SlackClient
   end
 
   def alert(text)
+    message("<!here> #{text}")
+  end
+
+  def message(text)
     uri = URI(slack_url)
-    message = "<!here> #{text}"
     icon_url = "#{base_url}/logo.jpg"
 
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
     req = Net::HTTP::Post.new(uri.path)
-    req.body = { text: message, username: "Caviar", icon_url: icon_url }.to_json
+    req.body = { text: text, username: "Caviar", icon_url: icon_url }.to_json
 
     https.request(req)
 
-    puts "Posting message to Slack channel: #{message}"
+    puts "Posting message to Slack channel: #{text}"
   end
 end
